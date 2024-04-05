68cb5ea37a612ee77a0e870a60faa2df88b2f332
Gustavo494-ux <53914350+Gustavo494-ux@users.noreply.github.com>
2024-04-05 05:50:45 -0300

    modulo 1 aula 5

--- src/componentes/Carrossel/index.js
+++ src/componentes/Carrossel/index.js
@@ -1,7 +1,29 @@
 import { FlatList, View, Image} from "react-native";
 import styles from './styles';
+import React, { useEffect, useRef, useState } from "react";
 
 export function Carrossel({data}){
+  const carrosselRef = useRef();
+  const [indice, setIndice] = useState(0)
+
+  function alteraPosicaoObjeto(){
+    if(indice < data.length - 1){
+      setIndice(indice + 1);
+    } else {
+      setIndice(0);
+    }
+  }
+
+  useEffect(() =>{
+    carrosselRef.current.scrollToIndex({index: indice })
+
+    const intervalo = setInterval(() => {
+      alteraPosicaoObjeto()
+    },2000)
+
+    return () => clearInterval(intervalo)
+  },[indice])
+
     return (
         <View style={styles.container}>
              <FlatList
@@ -9,13 +31,16 @@ export function Carrossel({data}){
               horizontal
               showsHorizontalScrollIndicator={false}
               keyExtractor={item => item.id}
-              renderItem={({ item }) => (
+              renderItem={({ item, index }) => (
                 <Image
                   source={item.imagem}
-                  style={{ height: '100%', width: 150 }}
+                  style={[
+                      { height: '100%', width: 150 }, 
+                      index == data.length-1 ? {marginRight: 200} : null]}
                   resizeMode="contain"
                 />
               )}
+              ref={carrosselRef}
             />
         </View>
     )
