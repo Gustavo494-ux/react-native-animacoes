e200a24bb96bdc4a65d543dd0a6a5dfcaa5ac503
Gustavo Gama <gustavogama494@gmail.com>
2024-04-05 16:41:41 -0300

    modulo 3 aula 2

--- src/telas/Detalhes/index.js
+++ src/telas/Detalhes/index.js
@@ -1,40 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, Image, TouchableOpacity, ScrollView, Button } from 'react-native';
 import { TelaDeFundo } from '../../componentes/TelaDeFundo';
 import { InformacoesUsuario } from '../../componentes/InformacoesUsuario';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import mapa from '../../assets/mapa.png';
 import styles from './styles';
-import Animated, {useSharedValue, useAnimatedStyle, withSpring} from 'react-native-reanimated';
+import Animated, {useSharedValue, useAnimatedStyle, withSpring, withTiming, withRepeat} from 'react-native-reanimated';
 
 
 export default function Detalhes(props) {
   const dados = props.route.params;
-
-  const posicao = useSharedValue(0);
+  const [jaAnimou, setJaAnimou] = useState(false)
+  const rotacao = useSharedValue(0);
+  const angulo = -30
   const estiloAnimado = useAnimatedStyle(() => {
     return{
       transform:[{
-          translateX: withSpring(posicao.value)          
+          rotate: `${rotacao.value}deg` 
         }]
     }
-  },[posicao])
+  },[rotacao])
 
-  function alterarPosicaoBloco(){
-    for (let index = 0; index < 21; index++) {
-      posicao.value = Math.random() * 300;      
-    }
+  function fazerRotacao(){
+      rotacao.value = withRepeat(withTiming(angulo, {duration: 100}),12,true)
+      setTimeout(() => {
+        setJaAnimou(true)
+      },2000)
   }
 
   return (
     <TelaDeFundo>
       <ScrollView showsVerticalScrollIndicator={false} style={styles.container}>
-        <Animated.View style={[{
-          backgroundColor: 'green',
-          width: 50,
-          height: 50
-        }, estiloAnimado]} />
-        <Button title='Mova' onPress={alterarPosicaoBloco} />
         <InformacoesUsuario
           nome={dados.nome}
           detalhes="Cliente desde 2018"
@@ -64,13 +60,16 @@ export default function Detalhes(props) {
           <Text>{dados.endereco}</Text>
           <TouchableOpacity 
             style={styles.botao} 
+            onPress={fazerRotacao}
           >
             <Text style={styles.botaoTexto}>Notificar consulta</Text>
-              <Icon 
-                name={'notifications-none'} 
-                size={20} 
-                color="#FFF"
-              />
+              <Animated.View style={estiloAnimado}>
+                <Icon 
+                  name={jaAnimou ? 'notifications' : 'notifications-none'} 
+                  size={20} 
+                  color="#FFF"
+                />
+              </Animated.View>
           </TouchableOpacity>
       </ScrollView>
     </TelaDeFundo>
